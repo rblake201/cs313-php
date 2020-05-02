192d2be020167a708bcbdeec02e88ef82a048282
Blake Pearson <blake.pearson159@gmail.com>
2020-05-02 16:00:07 -0700

    making progress on homepage and assignments page

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -1,3 +1,7 @@
+<?php
+    echo "Today is " . date("Y/m/d") . "<br>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +13,11 @@
 </head>
 <body>
     <h1>Homepage</h1>
-    <a href="assignments.html">Assignments</a>
+    <p class = "assign"><a href="assignments.html">Assignments</a></p>
+    <p class = "description">
+        I provided a Pokedex for my homepage. I thought it would be a fun way to show my interest in both Pokemon and web development. Try clicking one of the Pokemon...
+    </p>
+    <br>
     <div class="flex-container" id="pokedex"></div>
 </body>
 </html>
