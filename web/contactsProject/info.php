4bdfd66ad285f2f3596afd8b63be491e87e05304
Blake Pearson <blake.pearson159@gmail.com>
2020-05-23 16:56:00 -0700

    working on contacts project

--- web/contactsProject/info.php
+++ web/contactsProject/info.php
@@ -3,6 +3,39 @@
 require "databaseConnect.php";
 $db = getDb();
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Info</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            color: #588c7e;
+            font-family: monospace;
+            font-size: 25px;
+            text-align: left;
+        }
+        th {
+            background-color: #588c7e;
+            color: white;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
 echo "<table>
 <tr>
     <th>First Name</th>
