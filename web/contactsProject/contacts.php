c3e6652df68d750e4b17e1ed8a8dc998643b2f87
Blake Pearson <blake.pearson159@gmail.com>
2020-05-23 16:44:08 -0700

    working on contacts project

--- web/contactsProject/contacts.php
+++ web/contactsProject/contacts.php
@@ -6,7 +6,7 @@ $db = getDb();
 if(isset($_POST['search'])){
     $searchq = $_POST['search'];
 
-    $search = $db->query("SELECT * FROM contact WHERE first_name='Blake'");
+    $search = $db->query("SELECT first_name, last_name FROM contact WHERE first_name= '" . $searchq . "';");
     while ($row = $search->fetch(PDO::FETCH_ASSOC)) {
         echo "<tr><td>" . $row["first_name"]. "</td><td>" . $row["last_name"] . "</td></td>";
     }
