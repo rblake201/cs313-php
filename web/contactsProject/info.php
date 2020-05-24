36f40cc649b4a039e5d3e597f2b7746ce1e25da7
Blake Pearson <blake.pearson159@gmail.com>
2020-05-23 18:58:40 -0700

    working on contacts project

--- web/contactsProject/info.php
+++ web/contactsProject/info.php
@@ -87,6 +87,7 @@ if(isset($_POST['info'])){
         echo "<tr><td>" . $row["first_name"]. "</td><td>" . $row["last_name"] . "</td><td>" . $row["phone"] . "</td><td>" . $row["personal_email"] . "</td><td>"
         . $row["work_email"] . "</td><td>" . $row["facebook"] . "</td><td>" . $row["instagram"] . "</td><td>" . $row["discord"] . "</td></td>";
     }
+    echo "hello"
 }
 
 echo "</table>";
