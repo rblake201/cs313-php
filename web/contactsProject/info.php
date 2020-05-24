fc9ffd210f757817046fe2c121a4b2bb47bba61a
Blake Pearson <blake.pearson159@gmail.com>
2020-05-23 17:04:13 -0700

    working on contacts project

--- web/contactsProject/info.php
+++ web/contactsProject/info.php
@@ -30,45 +30,42 @@ $db = getDb();
     </style>
 </head>
 <body>
-    
+    <?php
+
+    echo "<table>
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Phone Number</th>
+        <th>Personal Email</th>
+        <th>Work Email</th>
+        <th>Facebook</th>
+        <th>Instagram</th>
+        <th>Discord</th>
+    </tr>";
+
+    if(isset($_POST['searchf'])){
+        $searchq = $_POST['searchf'];
+
+        $searchf = $db->query("SELECT * FROM contact AS u JOIN info AS n ON u.id = n.contact_id WHERE first_name= '" . $searchq . "';");
+        while ($row = $searchf->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr><td>" . $row["first_name"]. "</td><td>" . $row["last_name"] . "</td><td>" . $row["phone"] . "</td><td>" . $row["personal_email"] . "</td><td>"
+            . $row["work_email"] . "</td><td>" . $row["facebook"] . "</td><td>" . $row["instagram"] . "</td><td>" . $row["discord"] . "</td></td>";
+        }
+        echo "</table>";
+    }
+
+    if(isset($_POST['searchl'])){
+        $searchq = $_POST['searchl'];
+
+        $searchl = $db->query("SELECT * FROM contact AS u JOIN info AS n ON u.id = n.contact_id WHERE last_name= '" . $searchq . "';");
+        while ($row = $searchl->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr><td>" . $row["first_name"]. "</td><td>" . $row["last_name"] . "</td><td>" . $row["phone"] . "</td><td>" . $row["personal_email"] . "</td><td>"
+            . $row["work_email"] . "</td><td>" . $row["facebook"] . "</td><td>" . $row["instagram"] . "</td><td>" . $row["discord"] . "</td></td>";
+        }
+        echo "</table>";
+    }
+
+    ?>
 </body>
 </html>
-
-<?php
-
-echo "<table>
-<tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Phone Number</th>
-    <th>Personal Email</th>
-    <th>Work Email</th>
-    <th>Facebook</th>
-    <th>Instagram</th>
-    <th>Discord</th>
-</tr>";
-
-if(isset($_POST['searchl'])){
-    $searchq = $_POST['searchl'];
-
-    $searchl = $db->query("SELECT * FROM contact AS u JOIN info AS n ON u.id = n.contact_id WHERE last_name= '" . $searchq . "';");
-    while ($row = $searchl->fetch(PDO::FETCH_ASSOC)) {
-        echo "<tr><td>" . $row["first_name"]. "</td><td>" . $row["last_name"] . "</td><td>" . $row["phone"] . "</td><td>" . $row["personal_email"] . "</td><td>"
-        . $row["work_email"] . "</td><td>" . $row["facebook"] . "</td><td>" . $row["instagram"] . "</td><td>" . $row["discord"] . "</td></td>";
-    }
-    echo "</table>";
-}
-
-if(isset($_POST['searchf'])){
-    $searchq = $_POST['searchf'];
-
-    $searchf = $db->query("SELECT * FROM contact AS u JOIN info AS n ON u.id = n.contact_id WHERE first_name= '" . $searchq . "';");
-    while ($row = $searchf->fetch(PDO::FETCH_ASSOC)) {
-        echo "<tr><td>" . $row["first_name"]. "</td><td>" . $row["last_name"] . "</td><td>" . $row["phone"] . "</td><td>" . $row["personal_email"] . "</td><td>"
-        . $row["work_email"] . "</td><td>" . $row["facebook"] . "</td><td>" . $row["instagram"] . "</td><td>" . $row["discord"] . "</td></td>";
-    }
-    echo "</table>";
-}
-
-
-?>
