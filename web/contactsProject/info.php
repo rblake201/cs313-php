69a48671ac34bea6715278f29e9eb347759c101e
Blake Pearson <blake.pearson159@gmail.com>
2020-05-23 17:30:40 -0700

    working on contacts project

--- web/contactsProject/info.php
+++ web/contactsProject/info.php
@@ -48,7 +48,7 @@ echo "<table>
     <th>Discord</th>
 </tr>";
 
-if(isset($_POST['searchf'], $_POST['searchl'])){
+if(isset(($_POST['searchf'] and $_POST['searchf'] != ''), ($_POST['searchl'] and $_POST['searchf'] != ''))){
     $searchqf = $_POST['searchf'];
     $searchql = $_POST['searchl'];
 
