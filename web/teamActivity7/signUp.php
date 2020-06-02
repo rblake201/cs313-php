83ec926970b5b32f70fafcb3f008daa55ebad0c6
Blake Pearson <blake.pearson159@gmail.com>
2020-06-02 16:42:40 -0700

    working on signUp page

--- web/teamActivity7/signUp.php
+++ web/teamActivity7/signUp.php
@@ -9,7 +9,7 @@
 
 <h1>Sign Up Page</h1>
 
-<form action="signIn.php" method="post">
+<form action="signUp.php" method="post">
     <input type="hidden" name="posted" value=true>
     <label>Username<input type="text" name="user"></label><br>
     <label>Password<input type="text" name="pass"></label><br>
@@ -26,18 +26,13 @@ if($_POST['user'] != "" && $_POST['pass'] != "")
     $pass = $_POST['pass'];
 
     $user = htmlspecialchars($user);
-    $hashPass = password_hash($pass, PASSWORD_DEFAULT);
+    $hashpass = password_hash($pass, PASSWORD_DEFAULT);
 
-    require("\db_connect.php");
+    require("db_connect.php");
     $db = getDb();
 
-    $query = "INSERT INTO _user(username, pass) VALUES(:user, :pass);";
-    $statement = $db->prepare($query);
-
-    $statement->bindValue(':user', $user);
-    $statement->bindValue(':pass', $hashPass);
-    
-    $statement->execute();
+    $query = "INSERT INTO _user(username, pass) VALUES('".$user."', '".$hashpass."');";
+    $statement = $db->query($query);
 
     header("Location: signIn.php");
 
