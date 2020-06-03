333a5165bdf863701b1e94698bb873ecfb6ca232
Blake Pearson <blake.pearson159@gmail.com>
2020-06-02 20:03:12 -0700

    modified signUp

--- web/teamActivity7/signUp.php
+++ web/teamActivity7/signUp.php
@@ -10,8 +10,9 @@
 <h1>Sign Up Page</h1>
 
 <form action="signUp.php" method="post">
-    <label>Username<input type="text" name="user"></label><br>
-    <label>Password<input type="text" name="pass"></label><br>
+    <label>Username<input type="text" name="user" id="user"></label><br>
+    <label>Password<input type="text" name="pass" id="pass"></label><br>
+    <label>Re-enter Password<input type="text" name="pass1" id="pass1"></label><br>
     <input type="submit" value="Sign Up">
 </form>
 </body>
@@ -23,20 +24,23 @@ if(isset($_POST['user']) && isset($_POST['pass']))
 {
     if($_POST['user'] && $_POST['pass'])
     {
-        $user = $_POST['user'];
-        $pass = $_POST['pass'];
+        if($_POST['pass'] == $_POST['pass1'])
+        {
+            $user = $_POST['user'];
+            $pass = $_POST['pass'];
 
-        $user = htmlspecialchars($user);
-        $hashpass = password_hash($pass, PASSWORD_DEFAULT);
+            $user = htmlspecialchars($user);
+            $hashpass = password_hash($pass, PASSWORD_DEFAULT);
 
-        require("db_connect.php");
-        $db = getDb();
+            require("db_connect.php");
+            $db = getDb();
 
-        $query = "INSERT INTO _user(username, pass) VALUES('".$user."', '".$hashpass."');";
-        $statement = $db->query($query);
+            $query = "INSERT INTO _user(username, pass) VALUES('".$user."', '".$hashpass."');";
+            $statement = $db->query($query);
 
-        header("Location: signIn.php");
-        die();
+            header("Location: signIn.php");
+            die();
+        }
     }
 }
 
