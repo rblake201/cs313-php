72e766fa6ba5d0f9a370066ebdb551b65e611934
Blake Pearson <blake.pearson159@gmail.com>
2020-06-02 20:14:16 -0700

    modified signUp

--- web/teamActivity7/signUp.php
+++ web/teamActivity7/signUp.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        .red{
-            color: red;
-        }
-    </style>
-</head>
-<body>
-
-<h1>Sign Up Page</h1>
-
-<form action="signUp.php" method="post">
-    <label>Username<input type="text" name="user" id="user"></label><br>
-    <label>Password<input type="text" name="pass" id="pass"></label><br>
-    <label>Re-enter Password<input type="text" name="pass1" id="pass1"></label><br>
-    <input type="submit" value="Sign Up">
-</form>
-</body>
-</html>
-
 <?php
 
 if(isset($_POST['user']) && isset($_POST['pass']))
@@ -49,8 +24,37 @@ if(isset($_POST['user']) && isset($_POST['pass']))
         else
         {
             echo "<p class='red'>" . "Error" . "</p>";
+
+            $message = "*";
         }
     }
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .red{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Sign Up Page</h1>
+
+<form action="signUp.php" method="post">
+    <label>Username<input type="text" name="user" id="user">
+    <span class="red" <?php if(isset($message)){echo $message}?>></span></label><br>
+    <label>Password<input type="text" name="pass" id="pass">
+    <span class="red" <?php if(isset($message)){echo $message}?>></span></label><br>
+    <label>Re-enter Password<input type="text" name="pass1" id="pass1"></label><br>
+    <input type="submit" value="Sign Up">
+</form>
+</body>
+</html>
