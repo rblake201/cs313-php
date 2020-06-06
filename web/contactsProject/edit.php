a101d4af9def54e542179c924eddb149df232b3e
Blake Pearson <blake.pearson159@gmail.com>
2020-06-06 15:40:56 -0700

    adding edit functionality

--- web/contactsProject/edit.php
+++ web/contactsProject/edit.php
@@ -5,6 +5,44 @@ $db = getDb();
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+            input[type=text], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type=submit] {
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
+
 <form name="editform" action="edit.php" method="post">
     <input type="hidden" name="posted" value=true>
     <input type="text" name="addfn" placeholder="Enter first name..."/><br>
@@ -15,5 +53,5 @@ $db = getDb();
     <input type="text" name="addfan" placeholder="Enter facebook name..."/><br>
     <input type="text" name="addin" placeholder="Enter instagram name..."/><br>
     <input type="text" name="adddn" placeholder="Enter discord name..."/><br>
-    <input type="submit" value="Add"/>
+    <input type="submit" value="Save"/>
 </form>
