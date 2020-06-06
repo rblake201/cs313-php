887a30629e82eabafa3b7d79c67e3c18742325c8
Blake Pearson <blake.pearson159@gmail.com>
2020-06-06 16:51:11 -0700

    adding edit functionality

--- web/contactsProject/contacts.php
+++ web/contactsProject/contacts.php
@@ -182,11 +182,7 @@ $db = getDb();
 
             $result = $db->query($query);
 
-            header("Refresh:0.5");
-
-            echo '<script>alert("Contact Added!")</script>';
-
-            header("Refresh:0");
+            echo '<script>alert("Contact Added! Please refresh page")</script>';
 
         }
     }
@@ -197,10 +193,7 @@ $db = getDb();
         $delete = $db->query("DELETE FROM info WHERE contact_id= " . $searchq . ";");
         $delete1 = $db->query("DELETE FROM contact WHERE id= " . $searchq . ";");
 
-        header("Refresh:0.5");
+        echo '<script>alert("Contact Deleted! Please refresh page")</script>';
 
-        echo '<script>alert("Contact Deleted!")</script>';
-
-        header("Refresh:0");
     }
 ?>
