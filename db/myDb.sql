b3037d561da0bb279caf279ce89d7297fc98d7d1
Blake Pearson <blake.pearson159@gmail.com>
2020-06-06 17:28:04 -0700

    added inserts

--- db/myDb.sql
+++ db/myDb.sql
@@ -14,10 +14,21 @@ CREATE table contact (
 CREATE table info (
     id SERIAL PRIMARY KEY,
     contact_id INT NOT NULL REFERENCES contact(id),
-    phone VARCHAR(256) UNIQUE,
-    personal_email VARCHAR(256) UNIQUE,
-    work_email VARCHAR(256) UNIQUE,
-    facebook VARCHAR(256) UNIQUE,
-    instagram VARCHAR(256) UNIQUE,
-    discord VARCHAR(256) UNIQUE
+    phone VARCHAR(256),
+    personal_email VARCHAR(256),
+    work_email VARCHAR(256),
+    facebook VARCHAR(256),
+    instagram VARCHAR(256),
+    discord VARCHAR(256) 
 );
+
+--insert
+
+INSERT INTO contact (first_name, last_name) VALUES ('Blake', 'Pearson');
+INSERT INTO contact (first_name, last_name) VALUES ('John', 'Smith');
+
+INSERT INTO info (contact_id, phone, personal_email, work_email, facebook, instagram, discord)
+VALUES (1, '123-456-7890', 'blakepersonal@gmail.com', 'blakework@gmail.com', 'Blake Pearson', '@blakepearson', 'blakegamer');
+
+INSERT INTO info (contact_id, phone, personal_email, work_email, facebook, instagram, discord)
+VALUES (2, '098-765-4321', 'johnsmith@gmail.com', 'johnwork@gmail.com', 'John Smith', '@johnsmith', 'johngamer');
